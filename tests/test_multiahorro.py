0c5cba0cdf180c18f540ebc3a3120a9c235645b2
Javier Rey <javirey@gmail.com>
2013-02-16 00:38:25 -0200

    Se agrega test y arregla otro

--- tests/test_multiahorro.py
+++ tests/test_multiahorro.py
@@ -23,10 +23,16 @@ class TestMultiAhorroParser(unittest.TestCase):
         self.assertData("http://www.multiahorro.com.uy/Product.aspx?p=94295", "LECHE CONAP. FRESCA DESCR. 1L", "http://www.multiahorro.com.uy/images/Products/small/610912.jpg", ['Productos Frescos', 'Lacteos', 'Leche', 'Leche Varios'])
 
     def test_cebolla_value(self):
-        self.assertValue("http://www.multiahorro.com.uy/Product.aspx?p=106671", 36)
+        self.assertValue("http://www.multiahorro.com.uy/Product.aspx?p=106671", 29)
 
     def test_cebolla_data(self):
         self.assertData("http://www.multiahorro.com.uy/Product.aspx?p=106671", "CEBOLLA 1 Kg", "http://www.multiahorro.com.uy/images/Products/small/953150.jpg", ['Productos Frescos', 'Vegetales', 'Vegetales Varios'])
+
+    def test_maxi_mix_value(self):
+        self.assertValue("http://www.multiahorro.com.uy/Product.aspx?p=143292", 53)
+
+    def test_maxi_mix_data(self):
+        self.assertData("http://www.multiahorro.com.uy/Product.aspx?p=143292", "MAXI MIX WOLF 125G. 1 Kg", "http://www.multiahorro.com.uy/images/Products/small/634180.jpg", ["Alimentos Y Bebidas", "Alimentos", "Snaks", "Snaks Varios"])
 
 if __name__ == '__main__':
     unittest.main()
