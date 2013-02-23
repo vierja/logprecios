801c73e116baa57c82a75ad7158200185e2d6501
Javier Rey <javirey@gmail.com>
2013-02-23 19:21:25 -0200

    Faltaba testparser modificado

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,25 +5,16 @@ from tracker.tracker import get_parser
 
 class TestParser(unittest.TestCase):
 
-    def assertValue(self, url, value):
-        parser = get_parser(url)
-        site_value = parser.get_price()
-        assert site_value == value
-
-    def assertData(self, url, name, image_url, categories):
+    def assertData(self, url, name, price, image_url, categories):
         parser = get_parser(url)
         res = parser.get_data()
         assert res['name'] == name
+        assert res['price'] == price
         assert res['image_url'] == image_url
         assert all([x in res['categories'] for x in categories])
         assert all([x in categories for x in res['categories']])
 
-    def assertHostname(self, url, hostname):
-        parser = get_parser(url)
-        hostname_res = parser.url_is_valid()
-        assert hostname_res == hostname
-
     def assertMinification(self, url, minified):
         parser = get_parser(url)
-        minified_res = parser.minify()
+        minified_res = parser.minify_url()
         assert minified_res == minified
