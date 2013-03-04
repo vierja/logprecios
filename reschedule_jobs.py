e5789e6a8ca99413f767ef0d4334482538f9fe22
Javier Rey <javirey@gmail.com>
2013-03-04 00:02:12 -0200

    Fix para poder iterar sobre todos los productos sin quedarse sin memoria

--- reschedule_jobs.py
+++ reschedule_jobs.py
@@ -2,16 +2,26 @@ from datetime import datetime, timedelta
 from trackerapp import Product, db
 
 
+# Tomado de http://stackoverflow.com/a/1217947/310467
+def page_query(q):
+    offset = 0
+    while True:
+        r = False
+        for elem in q.limit(1000).offset(offset):
+            r = True
+            yield elem
+        offset += 1000
+        if not r:
+            break
+
+
 def reset_times():
-
     number_of_products = Product.query.count()
-
     second_interval = int(86400 / number_of_products)
-
     starting_time = datetime.utcnow()
     second_offset = 0
 
-    for product in Product.query.all():
+    for product in page_query(Product.query):
         scheduled_time = starting_time + timedelta(seconds=second_offset)
         product.tracking_time = scheduled_time
         db.session.add(product)
