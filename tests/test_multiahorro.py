34978a3aa3c87159d08c8b2da92acc1fe8acaa74
Javier Rey <javirey@gmail.com>
2013-04-27 13:47:04 -0300

    Update test_multiahorro.py

--- tests/test_multiahorro.py
+++ tests/test_multiahorro.py
@@ -8,7 +8,7 @@ class TestMultiAhorroParser(TestParser):
         self.assertData("http://www.multiahorro.com.uy/Product.aspx?p=94295", "LECHE CONAP. FRESCA DESCR. 1L", 16, "http://www.multiahorro.com.uy/images/Products/small/610912.jpg", ['Productos Frescos', 'Lacteos', 'Leche', 'Leche Varios'])
 
     def test_cebolla_data(self):
-        self.assertData("http://www.multiahorro.com.uy/Product.aspx?p=106671", "CEBOLLA", 36, "http://www.multiahorro.com.uy/images/Products/small/953150.jpg", ['Productos Frescos', 'Vegetales', 'Vegetales Varios'])
+        self.assertData("http://www.multiahorro.com.uy/Product.aspx?p=106671", "CEBOLLA", 38, "http://www.multiahorro.com.uy/images/Products/small/953150.jpg", ['Productos Frescos', 'Vegetales', 'Vegetales Varios'])
 
     def test_maxi_mix_data(self):
         self.assertData("http://www.multiahorro.com.uy/Product.aspx?p=143292", "MAXI MIX WOLF 125G.", 53, "http://www.multiahorro.com.uy/images/Products/small/634180.jpg", ["Alimentos Y Bebidas", "Alimentos", "Snaks", "Snaks Varios"])
